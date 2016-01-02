285277bc633dd2cd9006b046201369e1ba83e506
Gregory Kielian <gregory.kielian@gmail.com>
2016-01-02 20:36:30 +0100

    add todos and notes

--- Elbow_Theta_Phi/Elbow_Theta_Phi.ino
+++ Elbow_Theta_Phi/Elbow_Theta_Phi.ino
@@ -22,53 +22,66 @@ void setup() {
 }
 
 void loop() {
- singleMeasurementMode(); //device goes into low-power mode between measurements, until this is called again
- delay(6); //we must wait at least 6ms or monitor DRDY hardware interrupt pin
- read6bytes();
+  singleMeasurementMode(); //device goes into low-power mode between measurements, until this is called again
+  delay(6); //we must wait at least 6ms or monitor DRDY hardware interrupt pin
+  read6bytes();
 }
 
 
+//Register Helpers
+
+
+//set single measurement mode -- low-pwr mode entered between measurements
 void singleMeasurementMode() {
- send2Bytes(0x02, 0x01);
+  send2Bytes(0x02, 0x01);
 }
+
+//set continuous measurement mode -- faster than sngle measurement mode, I haven't tested how much faster yet
 void continuousMeasurementMode() {
- send2Bytes(0x02, 0x00);
+  send2Bytes(0x02, 0x00);
 }
+
+
+//sets gain for all channels, input must be between 1->8
 void gain(byte gain) {
- send2Bytes(0x01, gain << 5); //shift 5 bytes left, as leftmost 3 registers are the targets
+  send2Bytes(0x01, gain << 5); //shift 5 bytes left, as leftmost 3 registers are the targets
 }
+
+//TODO needs a better name, as we also use this for setting the data output rate
 void average8() {
-  
- send2Bytes(0x00, AVERAGE_8 | DO_75HZ); //average 8 samples before data is output, data output rate of 75Hz
+  send2Bytes(0x00, AVERAGE_8 | DO_75HZ); //average 8 samples before data is output, data output rate of 75Hz
 }
 
 
-//helper functions
+//wire-wrapper functions (rofl, wire-wrapper...)
+
 void send2Bytes(byte x, byte y) {
- Wire.beginTransmission(i2c_addr);
- Wire.write(x);
- Wire.write(y);
- Wire.endTransmission();
+  Wire.beginTransmission(i2c_addr);
+  Wire.write(x);
+  Wire.write(y);
+  Wire.endTransmission();
 }
 
+//right now we print these but need not
 void read6bytes() {
- Wire.beginTransmission(i2c_addr);
- Wire.write(0x03);
- Wire.endTransmission();
+  Wire.beginTransmission(i2c_addr);
+  Wire.write(0x03);
+  Wire.endTransmission();
 
- Wire.requestFrom(i2c_addr,0x06);
- if(0x06 <= Wire.available()){
-   x = Wire.read()<<8; //x's msb
-   x |= Wire.read(); //x's lsb
-   y = Wire.read()<<8; //y's msb
-   y |= Wire.read(); //y's lsb
-   z = Wire.read()<<8; //z's msb
-   z |= Wire.read(); //z's lsb
- }
+  Wire.requestFrom(i2c_addr,0x06);
+  if(0x06 <= Wire.available()){
+    x = Wire.read()<<8; //x's msb
+    x |= Wire.read(); //x's lsb
+    y = Wire.read()<<8; //y's msb
+    y |= Wire.read(); //y's lsb
+    z = Wire.read()<<8; //z's msb
+    z |= Wire.read(); //z's lsb
+  }
 
- Serial.print("x = "); Serial.print(x);
- Serial.print("  y = "); Serial.print(y);
- Serial.print("  z = "); Serial.println(z);
+  //I like the adafruit style arduino prints
+  Serial.print("x = "); Serial.print(x);
+  Serial.print("  y = "); Serial.print(y);
+  Serial.print("  z = "); Serial.println(z);
 
- delay(16); //approximately 60Hz, far within our 75Hz data output rate envelope
+  delay(16); //approximately 60Hz, far within our 75Hz data output rate envelope
 }
